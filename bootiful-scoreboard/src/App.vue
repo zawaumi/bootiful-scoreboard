87345abba222b801730376aa3780040284f67c81
pnia0 <tryo2357@gmail.com>
2025-10-05 18:28:29 +0900

    feat: setup bootstrap and create basic structure.

--- bootiful-scoreboard/src/App.vue
+++ bootiful-scoreboard/src/App.vue
@@ -1,30 +1,102 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="w-100 row m-0">
+      <h1 class="col-md-12 text-center">ランキング</h1>
+      <!--1~9位まで-->
+      <div class="d-flex flex-column align-items-center col-6 p-5">
+        <div v-for="index in 9" class="w-100 m-2 border border-1 rounded shadow">
+          <h3>{{ index }} 位</h3>
+          <div class="d-flex flex-row justify-content-center"><h5 class="flex-grow-1 text-end">{{ teams[index - 1].name }}</h5><h5 class="px-2">|</h5><h5 class="flex-grow-1 text-start">score: {{ teams[index - 1].score }}</h5></div>
+          <h5> </h5>
+        </div>
+      </div>
+      <!--10位以降-->
+      <div class="d-flex flex-column align-items-center col-6 p-5">
+        <div v-for="(team, index) in teams">
+          <div v-if="9 <= index">
+            {{ index + 1 }} 位{{ team.name }}score: {{ team.score }}</div>
+          </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      teams: [
+        {
+          name: "test 1",
+          score: "2",
+        },
+        {
+          name: "test 2",
+          score: "4",
+        },
+        {
+          name: "test 3",
+          score: "5",
+        },
+        {
+          name: "test 4",
+          score: "5",
+        },
+        {
+          name: "test 5",
+          score: "5",
+        },
+        {
+          name: "test 6",
+          score: "5",
+        },
+        {
+          name: "test 7",
+          score: "5",
+        },
+        {
+          name: "test 8",
+          score: "5",
+        },
+        {
+          name: "test 9",
+          score: "5",
+        },
+        {
+          name: "test 10",
+          score: "5",
+        },
+        {
+          name: "test 11",
+          score: "5",
+        },
+        {
+          name: "test 12",
+          score: "5",
+        },
+        {
+          name: "test 13",
+          score: "5",
+        },
+        {
+          name: "test 14",
+          score: "5",
+        },
+        {
+          name: "test 15",
+          score: "5",
+        },
+        {
+          name: "test 16",
+          score: "5",
+        },
+      ]
+    }
+  },
+}
+</script>
+
+
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
