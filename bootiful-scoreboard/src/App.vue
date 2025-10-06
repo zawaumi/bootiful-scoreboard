efb63e6613051b7b29306c7c70fdd006a8660a96
pnia0 <tryo2357@gmail.com>
2025-10-06 18:25:24 +0900

    feat(App.vue): install firebase, remove testdata, and implement feature that get score data.

--- bootiful-scoreboard/src/App.vue
+++ bootiful-scoreboard/src/App.vue
@@ -6,15 +6,16 @@
       <div class="d-flex flex-column align-items-center col-6 p-5">
         <div v-for="index in 9" class="w-100 m-2 border border-1 rounded shadow">
           <h3>{{ index }} 位</h3>
-          <div class="d-flex flex-row justify-content-center"><h5 class="flex-grow-1 text-end">{{ teams[index - 1].name }}</h5><h5 class="px-2">|</h5><h5 class="flex-grow-1 text-start">score: {{ teams[index - 1].score }}</h5></div>
-          <h5> </h5>
+          <div v-if="(teams[index-1] ?? false)">
+            <div class="d-flex flex-row justify-content-center"><h5 class="flex-grow-1 text-end">{{ teams[index - 1].data().roomId ?? UNKNOWN }}</h5><h5 class="px-2">|</h5><h5 class="flex-grow-1 text-start">score: {{ teams[index - 1].data().score ?? 0 }}</h5></div>
+          </div>
         </div>
       </div>
       <!--10位以降-->
       <div class="d-flex flex-column align-items-center col-6 p-5">
         <div v-for="(team, index) in teams">
           <div v-if="9 <= index">
-            {{ index + 1 }} 位{{ team.name }}score: {{ team.score }}</div>
+            {{ index + 1 }} 位{{ team.data().roomId }}score: {{ team.data().score }}</div>
           </div>
       </div>
     </div>
@@ -22,75 +23,44 @@
 </template>
 
 <script>
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { collection, getFirestore, onSnapshot } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUPmxyioDa0pwQO_enfiv62Gb_v4HG9i0",
+  authDomain: "c4sbootiful.firebaseapp.com",
+  databaseURL: "https://c4sbootiful-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "c4sbootiful",
+  storageBucket: "c4sbootiful.firebasestorage.app",
+  messagingSenderId: "1006642710358",
+  appId: "1:1006642710358:web:951265dd7e9e87e0c578f7",
+  measurementId: "G-ZNQGLVHD37"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+const db_scores = collection(db, "scores");
+
 export default {
   data() {
     return {
-      teams: [
-        {
-          name: "test 1",
-          score: "2",
-        },
-        {
-          name: "test 2",
-          score: "4",
-        },
-        {
-          name: "test 3",
-          score: "5",
-        },
-        {
-          name: "test 4",
-          score: "5",
-        },
-        {
-          name: "test 5",
-          score: "5",
-        },
-        {
-          name: "test 6",
-          score: "5",
-        },
-        {
-          name: "test 7",
-          score: "5",
-        },
-        {
-          name: "test 8",
-          score: "5",
-        },
-        {
-          name: "test 9",
-          score: "5",
-        },
-        {
-          name: "test 10",
-          score: "5",
-        },
-        {
-          name: "test 11",
-          score: "5",
-        },
-        {
-          name: "test 12",
-          score: "5",
-        },
-        {
-          name: "test 13",
-          score: "5",
-        },
-        {
-          name: "test 14",
-          score: "5",
-        },
-        {
-          name: "test 15",
-          score: "5",
-        },
-        {
-          name: "test 16",
-          score: "5",
-        },
-      ]
+      teams: []
+    }
+  },
+  created() {
+      const unh = onSnapshot(db_scores, (snapshot) => {
+        this.teams = this.sort_teams(snapshot.docs);
+      });
+  },
+  methods: {
+    sort_teams(score){
+      return score;
     }
   },
 }
